ab45fd3424322c78cadc9348509a7560f4654ec2
Jae Min Ko <jvc123456@khu.ac.kr>
2022-04-02 17:36:46 +0900

    feat : TCP echo client 구현

---
+++ SocketNetwork/Node/tcp_echo_client.mjs
@@ -0,0 +1,45 @@
+import net from 'net';
+import readline from 'readline';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const client = net.createConnection({port:PORT,host:HOST});
+client.setDefaultEncoding('utf8');
+    
+client.on("connect",()=>{
+    console.log("Connect to Server!\n");
+    
+    rl.on("line",(msg)=>{
+        if(msg==='exit'||msg==='quit'||msg==='end'){
+            client.end()
+            console.log("Exit Chatting Room")
+        }
+        client.write(msg)
+    })
+});
+client.on("data", (data) => {
+    console.log(data.toString());
+});
+
+client.on("end",(haderror)=>{
+    if(haderror===true) throw "Error";
+    console.log("> Server will be closed")
+    client.end()
+})
+
+//socket이 완전히 닫혔을때
+client.on("close",(haderror)=>{
+    if(haderror===true) throw "Error";
+
+    console.log("> End")
+    process.exit(1);
+})
